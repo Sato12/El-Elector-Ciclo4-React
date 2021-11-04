10b97b5709d49f0ba81305da449f4bd4211ab2bc
Santiago <segonzalezu@unal.edu.co>
2021-11-03 21:43:28 -0500

    Avance a la verson 6 de reac-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Candidatos from "./components/candidatos/Candidatos";
 import Home from "./components/misc/Home";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/candidatos" component={Candidatos} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/candidatos" element={<Candidatos />} />
+      </Routes>
     </Router>
   );
 }
