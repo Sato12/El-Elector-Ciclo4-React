2652ef1ca0f9b00bbe4d713f27a598a69718aaba
Sato12 <satugo0866@hotmail.com>
2021-11-21 11:13:44 -0500

    o

--- src/components/login/Registro.js
+++ src/components/login/Registro.js
@@ -1,123 +1,124 @@
-import React,{useState, useContext} from 'react';
-import authContext from '../../context/auth/authContext';
+import React, { useState, useContext, useEffect } from "react";
+import authContext from "../../context/auth/authContext";
+import { useNavigate } from "react-router-dom";
 
+const Registro = () => {
+  const authContextt = useContext(authContext);
+  const { registrarUsuario, autenticado } = authContextt;
 
-const Registro = ()=>{
+  const navigate = useNavigate();
 
-    const authContextt = useContext(authContext);
-    const {registrarUsuario} = authContextt;
+  useEffect(() => {
+    if (autenticado) {
+      navigate("/candidatos");
+    }
+  }, []);
 
-    const [user, setUser] = useState({
-        nombre: "",
-        email: "",
-        password: "",
-        confirmar: "",
-      });
+  const [user, setUser] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+    confirmar: "",
+  });
 
-    const { nombre, email, password, confirmar } = user;
+  const { nombre, email, password, confirmar } = user;
 
-    const handleOnChange = (e) =>{
-        setUser({
-            ...user,
-            [e.target.name] : e.target.value,
-        });
-    };
+  const handleOnChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleSubmit = (e)=>{
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        if (
-            nombre.trim() === "" ||
-            email.trim() === "" ||
-            password.trim() === "" ||
-            confirmar.trim() === ""
-          ) {
-            console.log("Todos los campos son obligatorios...");
-            return;
-          }
-        
-        if (password !== confirmar) {
-            console.log("Las contraseñas no coinciden");
-            return;
-        }
+    if (
+      nombre.trim() === "" ||
+      email.trim() === "" ||
+      password.trim() === "" ||
+      confirmar.trim() === ""
+    ) {
+      console.log("Todos los campos son obligatorios...");
+      return;
+    }
 
-        // Mandarlo al dispatch
+    if (password !== confirmar) {
+      console.log("Las contraseñas no coinciden");
+      return;
+    }
 
-        registrarUsuario({
-            nombre,
-            email, 
-            password
-        });
+    // Mandarlo al dispatch
 
+    registrarUsuario({
+      nombre,
+      email,
+      password,
+    });
+  };
 
-    };
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1 className="mb-5">Crea tu cuenta</h1>
 
-    return(
-        <div className="form-usuario">
-        <div className="contenedor-form sombra-dark">
-          <h1 className="mb-5">Crea tu cuenta</h1>
-
-          <form onSubmit={handleSubmit}>
-            <div className="campo-form">
-              <label htmlFor="nombre">Nombre</label>
-              <input
-                type="text"
-                id="nombre"
-                name="nombre"
-                value={nombre}
-                placeholder="Juan Perez"
-                onChange={handleOnChange}
-
-              />
-            </div>
-            <div className="campo-form">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                placeholder="email@email.com"
-                onChange={handleOnChange}
-              />
-            </div>
-            <div className="campo-form">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="password123"
-                value={password}
-                onChange={handleOnChange}
-              />
-            </div>
-            <div className="campo-form">
-              <label htmlFor="confirmar">Repite tu password</label>
-              <input
-                type="password"
-                id="confirmar"
-                name="confirmar"
-                placeholder="password123"
-                value={confirmar}
-                onChange={handleOnChange}
-              />
-            </div>
-            <div className="campo-form">
-              <input
-                type="submit"
-                className="btnn btnn-primario btnn-block"
-                value="Registrarse"
-              />
-            </div>
-          </form>
-          <a>
-            Volver a iniciar sesión
-          </a>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="campo-form">
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              name="nombre"
+              value={nombre}
+              placeholder="Juan Perez"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="email@email.com"
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password123"
+              value={password}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="confirmar">Repite tu password</label>
+            <input
+              type="password"
+              id="confirmar"
+              name="confirmar"
+              placeholder="password123"
+              value={confirmar}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btnn btnn-primario btnn-block"
+              value="Registrarse"
+            />
+          </div>
+        </form>
+        <a>Volver a iniciar sesión</a>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Registro;
-
--- src/context/auth/authState.js
+++ src/context/auth/authState.js
@@ -1,61 +1,57 @@
-import React,{useReducer} from 'react'
+import React, { useReducer } from "react";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
 import clienteAxios from "../../config/axios";
-import {REGISTRO_EXITOSO, 
-      REGISTRO_ERROR,
-      OBTENER_USUARIO,
-      LOGIN_EXITOSO,
-      LOGIN_ERROR,
-      CERRAR_SESION
+import {
+  REGISTRO_EXITOSO,
+  REGISTRO_ERROR,
+  OBTENER_USUARIO,
+  LOGIN_EXITOSO,
+  LOGIN_ERROR,
+  CERRAR_SESION,
 } from "../../types";
 
-const AuthState = (props)=>{
+const AuthState = (props) => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    autenticado: null,
+    usuario: null,
+    mensaje: null,
+  };
 
-    const initialState = {
-        token: localStorage.getItem("token"),
-        autenticado: null,
-        usuario: null,
-        mensaje: null,
-    };
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
-    const [state, dispatch] = useReducer(authReducer, initialState);
+  const registrarUsuario = async (datos) => {
+    try {
+      const rta = await clienteAxios.post("/api/usuarios", datos);
+      console.log(rta);
 
-    
-    const registrarUsuario = async datos =>{
-        try {
+      dispatch({
+        type: REGISTRO_EXITOSO,
+        payload: rta.data,
+      });
+    } catch (error) {
+      console.log(error);
 
-            const rta = await clienteAxios.post("/api/usuarios", datos);
-            console.log(rta);
+      dispatch({
+        type: REGISTRO_ERROR,
+      });
+    }
+  };
 
-            dispatch({
-                type: REGISTRO_EXITOSO
-            })
-            
-        } catch (error) {
-            console.log(error);
-
-            dispatch({
-                type: REGISTRO_ERROR
-            });
-        }
-    };
-
-    return(
-        <AuthContext.Provider
-            value={{
-                token: state.token,
-                autenticado: state.autenticado,
-                usuario: state.usuario,
-                mensaje: state.mensaje,
-                registrarUsuario,
-            }}
-        >
-            {props.children}
-        </AuthContext.Provider>
-    );
-
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        autenticado: state.autenticado,
+        usuario: state.usuario,
+        mensaje: state.mensaje,
+        registrarUsuario,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthState;
-
